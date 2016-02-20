4b46c57626eb0d82def93b9b3b9a7f572f986c07
aliia_kaiumova <aliia.kaiumova@gmail.com>
2016-02-20 19:25:30 +0600

    Modified question controller and question edit and show views.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,60 @@
 class QuestionsController < ApplicationController
+  add_breadcrumb 'Tests', :tests_url
+
   def index
+    @questions = Question.all
   end
 
-  def new
+  def show
+    #@test = Test.find(params[:test_id])
+    @question = Question.find(params[:id])
+
+  end
+
+  def create
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.create(question_params)
+
+    if @question.save
+      flash[:success] = 'Question has been added successfully'
+      redirect_to test_url(@test)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.find(params[:id])
+    add_breadcrumb @test.name, :test_url
+    add_breadcrumb 'Edit question', :edit_test_question_url
+  end
+
+  def update
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to test_url(@test)
+      flash[:success] = '?????? ???????????? ??????? ?????????'
+    else
+      flash[:danger] = '?? ????? ???????????? ??????, ????????? ? ?????????? ?????'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.find(params[:id])
+
+    @question.destroy
+    flash[:success] = '???????????? ??????? ??????'
+    redirect_to test_url(@test)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :test_id)
   end
 end
