707a6904134634f25d1ebb59488248cc4a7421f8
aliia_kaiumova <aliia.kaiumova@gmail.com>
2016-02-20 13:13:28 +0600

    Created different layout for sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :resolve_layout
+
+  private
+
+  def resolve_layout
+    if devise_controller? && resource_name == :user
+      'sign_in'
+    else
+      'application'
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    authenticated_root_path
+  end
 end
