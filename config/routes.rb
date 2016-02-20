339377caabc215e639cfb17e77347fee5f650a71
aliia_kaiumova <aliia.kaiumova@gmail.com>
2016-02-20 13:11:12 +0600

    Added root path for authenticated and unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    authenticated :user do
+      root to: 'tests#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :tests
 end
